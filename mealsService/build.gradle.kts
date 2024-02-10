f33dcbe07fa09b3d1b7bf15d0d65f1a566d93edc
Praveen <chintap94@gmail.com>
2024-02-09 20:38:48 -0600

    Fixed build issues.

--- mealsService/build.gradle.kts
+++ mealsService/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application")
+    id("com.android.library")
     id("org.jetbrains.kotlin.android")
 }
 
@@ -7,31 +7,13 @@ android {
     namespace = "com.receipes.mealsservice"
     compileSdk = 34
 
-    defaultConfig {
-        applicationId = "com.receipes.mealsservice"
-        minSdk = 29
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
+
+    buildFeatures {
+        buildConfig = true
     }
 }
 
